6f176c53137f54760a0f917b2697226805e64316
Nick <nicholasmink@outlook.com>
2017-12-08 14:25:53 -0600

    adding some old stuff

---
+++ Musician App/app/src/main/java/com/nicholasmink/mossman/MainActivity.java
@@ -0,0 +1,38 @@
+package com.nicholasmink.mossman;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView mywebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mywebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings= mywebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mywebView.loadUrl("https://mossman-c2dcc.firebaseapp.com/");
+        // Line of Code for opening links in app
+        mywebView.setWebViewClient(new WebViewClient());
+
+    }
+
+    //Code For Back Button
+    @Override
+    public void onBackPressed() {
+        if(mywebView.canGoBack())
+        {
+            mywebView.goBack();
+        }
+
+        else
+        {
+            super.onBackPressed();
+        }
+    }
+}
